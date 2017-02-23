ccd368f10a42f8aa9e7b74545f0504ed98395394
Linus <liquan1992@outlook.com>
2017-02-23 11:05:58 +0800

    Modified to deploy on heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -7,6 +7,6 @@ var mongoose = require('mongoose');
  * Mongoose will not throw any errors by default if you use a model without connecting.
  */
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
