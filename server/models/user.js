2fc4596bd0895f1b276d049e6c78bcf297e65b5b
Linus <liquan1992@outlook.com>
2017-02-24 15:31:57 +0800

    add private route for authetication

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,81 @@
-var mongoose = require('mongoose'); // don't need to require mongoose.js
+const mongoose = require('mongoose');
+const validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
-var User = mongoose.model('User', {
-    email:{
-        type:String,
+var UserSchema = new mongoose.Schema({
+    email: {
+        type: String,
         required: true,
         trim: true,
-        minlength: 1
-    }
+        minlength: 1,
+        unique: true,
+        validate: {
+            validator: validator.isEmail,
+            message: '{VALUE} is not a valid email'
+        }
+    },
+    password: {
+        type: String,
+        require: true,
+        minlength: 6
+    },
+    tokens: [{
+        access: {
+            type: String,
+            required: true
+        },
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 });
 
-module.exports= {User};
+//override its toJSON method
+UserSchema.methods.toJSON = function () {
+    var user = this;
+    //Returns an object with each property name and value corresponding to the entries in this collection.
+    var userObject = user.toObject();
+
+    return _.pick(userObject, ['_id', 'email']);
+};
+
+UserSchema.methods.generateAuthToken = function () {
+    var user = this;
+    var access = 'auth';
+    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+    user.tokens.push({access, token});
+    //save the user in db
+    return user.save().then(() => {
+        // return the token and chain promise if onto this is valid
+        return token;
+    });
+};
+
+// static(model) method
+UserSchema.statics.findByToken = function (token) {
+    var User = this; //Model method gets called
+    var decoded;
+
+    try {
+        decoded = jwt.verify(token, 'abc123');
+    } catch (e) {
+        // catch error if token verification fails
+        return Promise.reject({e});
+    }
+
+    // query
+    return User.findOne({
+        '_id': decoded._id,
+        'tokens.token': token,
+        'tokens.access': 'auth'
+    });
+};
+
+
+
+var User = mongoose.model('User', UserSchema);
+
+module.exports = {User}
--- server/server.js
+++ server/server.js
@@ -9,7 +9,9 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authentication');
 
+// load service
 var fileService = require('./service/file-service');
 
 
@@ -103,14 +105,35 @@ app.patch('/todos/:id', (req, res) => {
     })
 });
 
+
+// POST public
+app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then((token) => {
+        res.header('x-auth', token).send(user);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+});
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
+
 //image upload
-app.post('/upload_img', function (req, res) {
+app.post('/upload_img', (req, res) => {
     if (!req.files)
-        return res.status(400).send({status: "FAIL", error:'file doest not exist'});
+        return res.status(400).send({status: "FAIL", error: 'file doest not exist'});
 
     let file = req.files.uploadfile;
 
-    fileService.saveFile(file, __dirname + '/public/img/', ['jpg','png','jpeg']).then((result) => {
+    fileService.saveFile(file, __dirname + '/public/img/', ['jpg', 'png', 'jpeg']).then((result) => {
         result.status = 'SUCCESS';
         res.send(result);
     }, (err) => {
@@ -119,6 +142,8 @@ app.post('/upload_img', function (req, res) {
     });
 
 });
+
+
 
 app.listen(port, () => {
     console.log(`Started up at port ${port}`);
