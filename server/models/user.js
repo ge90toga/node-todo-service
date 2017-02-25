49b8c897c41187997274761d4c0109ff99391143
Linus <liquan1992@outlook.com>
2017-02-25 11:03:39 +0800

    set the config file

--- server/models/user.js
+++ server/models/user.js
@@ -45,7 +45,7 @@ UserSchema.methods.toJSON = function () {
 UserSchema.methods.generateAuthToken = function () {
     var user = this;
     var access = 'auth';
-    var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+    var token = jwt.sign({_id: user._id.toHexString(), access}, process.env.JWT_SECRET).toString();
 
     user.tokens.push({access, token});
     //save the user in db
@@ -61,7 +61,7 @@ UserSchema.statics.findByToken = function (token) {
     var decoded;
 
     try {
-        decoded = jwt.verify(token, 'abc123');
+        decoded = jwt.verify(token, process.env.JWT_SECRET);
     } catch (e) {
         // catch error if token verification fails
         return Promise.reject({e});
